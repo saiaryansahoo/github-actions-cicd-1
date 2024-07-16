dbd91f3167cbd40b2bbafa32b37ef0259896e53a
Sai Aryan Sahoo <saiaryan.sahoo@gmail.com>
2024-07-17 01:22:29 +0530

    Update addition.java

--- src/addition.java
+++ src/addition.java
@@ -1,8 +1,9 @@
-class Solution{
+public class addition{
   public static void main(String[] args){
     int a=5;
     int b=2;
     int result=a+b;
     System.out.println(result);
+    System.out.println("Successfully Running");
   }
 }
